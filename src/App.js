9d2086a56a6c3404e7ba52efcec4023d14b50720
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-10-28 01:20:52 +0700

    [init] style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { styled } from "@mui/material/styles";
+
+const Div = styled("div")(({ theme }) => ({
+  ...theme.typography.h2,
+  color: "white",
+}));
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      <Div>{"iFlame Recommendation"}</Div>;
+    </div>
+  );
 }
 
 export default App;
