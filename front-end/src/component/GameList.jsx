9adc7ddb6b2dc81bc07be7e7fcf0984b4e5d6156
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-11-02 22:56:37 +0700

    [EDIT] comment unused function

--- front-end/src/component/GameList.jsx
+++ front-end/src/component/GameList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
+// import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import {
   Button,
